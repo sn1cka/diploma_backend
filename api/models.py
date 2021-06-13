6de590ff2fbdd9389d6d3b00e49b97d11b7368f3
sn1cka <ilenko.a99@gmail.com>
2021-06-14 00:24:17 +0600

    Added extra fields +Reworked serializers

--- api/models.py
+++ api/models.py
@@ -1,97 +1,103 @@
 from django.db import models
 
-
 # Create your models here.
 from api.choices import tour_difficulties
 
 
 class Tour(models.Model):
-	photo = models.ImageField(verbose_name="Фото", upload_to="tour_images")
-	name = models.CharField(max_length=512, verbose_name="Название")
-	region = models.CharField(max_length=1024, verbose_name="Регион")
+    photo = models.ImageField(verbose_name="Фото", upload_to="tour_images")
+    name = models.CharField(max_length=100, verbose_name="Название")
+    region = models.CharField(max_length=100, verbose_name="Регион")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = "Тур"
-		verbose_name_plural = "Туры"
+    class Meta:
+        verbose_name = "Тур"
+        verbose_name_plural = "Туры"
 
 
 class TourPhoto(models.Model):
-	tour = models.ForeignKey(Tour, related_name="photos", on_delete=models.CASCADE)
-	photo = models.ImageField(verbose_name="Фото", upload_to="tour_images")
+    tour = models.ForeignKey(Tour, related_name="photos", on_delete=models.CASCADE)
+    photo = models.ImageField(verbose_name="Фото", upload_to="tour_images")
 
-	class Meta:
-		verbose_name = "Фото тура"
-		verbose_name_plural = "Фотографии тура"
+    class Meta:
+        verbose_name = "Фото тура"
+        verbose_name_plural = "Фотографии тура"
 
 
 class CompanyContacts(models.Model):
-	instagram = models.CharField(max_length=255, verbose_name="Instagram", null=True, blank=True)
-	telegram = models.CharField(max_length=255, verbose_name="Telegram", null=True, blank=True)
-	whatsapp = models.CharField(max_length=255, verbose_name="WhatsApp", null=True, blank=True)
-	phone = models.CharField(max_length=255, verbose_name="Phone", null=True, blank=True)
+    instagram = models.CharField(max_length=255, verbose_name="Instagram", null=True, blank=True)
+    telegram = models.CharField(max_length=255, verbose_name="Telegram", null=True, blank=True)
+    whatsapp = models.CharField(max_length=255, verbose_name="WhatsApp", null=True, blank=True)
+    phone = models.CharField(max_length=255, verbose_name="Phone", null=True, blank=True)
 
-	class Meta:
-		verbose_name = "Контакты компании"
-		verbose_name_plural = "Контакты компаний"
+    class Meta:
+        verbose_name = "Контакты компании"
+        verbose_name_plural = "Контакты компаний"
 
 
 class Company(models.Model):
-	name = models.CharField(max_length=512, verbose_name="Название")
-	contacts = models.OneToOneField(CompanyContacts, on_delete=models.CASCADE, verbose_name="Контакты", related_name="company")
+    name = models.CharField(max_length=100, verbose_name="Название")
+    contacts = models.OneToOneField(CompanyContacts, on_delete=models.CASCADE, verbose_name="Контакты",
+                                    related_name="company")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = "Компания"
-		verbose_name_plural = "Компании"
+    class Meta:
+        verbose_name = "Компания"
+        verbose_name_plural = "Компании"
 
 
 class CompanyFeed(models.Model):
-	company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="Компания", related_name="feeds")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="Компания", related_name="feeds")
 
-	name = models.CharField(max_length=512, verbose_name="Название")
-	photo = models.ImageField(verbose_name="Фото", upload_to="feed_images")
-	feed = models.CharField(max_length=1024, verbose_name="Отзыв")
+    name = models.CharField(max_length=200, verbose_name="Название")
+    photo = models.ImageField(verbose_name="Фото", upload_to="feed_images")
+    feed = models.CharField(max_length=1024, verbose_name="Отзыв")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	class Meta:
-		verbose_name = "Отзыв о компании"
-		verbose_name_plural = "Отзывы о компании"
+    class Meta:
+        verbose_name = "Отзыв о компании"
+        verbose_name_plural = "Отзывы о компании"
 
 
 class TourVariant(models.Model):
-	tour = models.ForeignKey(Tour, verbose_name="Тур", related_name="variants", on_delete=models.CASCADE)
-	company = models.ForeignKey(Company, verbose_name="Компания", related_name="variants", on_delete=models.CASCADE)
-	coast = models.PositiveIntegerField(verbose_name="Цена")
-	date = models.DateField(verbose_name="Дата")
+    tour = models.ForeignKey(Tour, verbose_name="Тур", related_name="variants", on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, verbose_name="Компания", related_name="variants", on_delete=models.CASCADE)
 
-	difficulty = models.CharField(max_length=255, verbose_name="Сложность", choices=tour_difficulties)
-	out_time = models.DateTimeField(verbose_name="Время выхода")
-	back_time = models.DateTimeField(verbose_name="Время возвращения")
-	photographer = models.BooleanField(verbose_name="Наличие фотографа")
+    coast = models.PositiveIntegerField(verbose_name="Цена")
+    date = models.DateField(verbose_name="Дата")
+    difficulty = models.CharField(max_length=100, verbose_name="Сложность", choices=tour_difficulties)
+    out_time = models.DateTimeField(verbose_name="Время выхода")
+    back_time = models.DateTimeField(verbose_name="Время возвращения")
+    photographer = models.BooleanField(verbose_name="Наличие фотографа")
+    start_height = models.PositiveIntegerField(verbose_name="Высота старта")
+    max_height = models.PositiveIntegerField(verbose_name="Максимальная высота")
+    days_count = models.PositiveIntegerField(verbose_name="Количество дней")
+    path_length_m = models.PositiveIntegerField(verbose_name="Длина пешего пути (м)")
+    needed_items = models.CharField(max_length=1024, verbose_name="Необходимые вещи")
 
-	def __str__(self):
-		return f"{self.tour.name} - {self.company.name}"
 
-	class Meta:
-		verbose_name = "Вариант тура"
-		verbose_name_plural = "Варианты туров"
+    def __str__(self):
+        return f"{self.tour.name} - {self.company.name}"
+
+    class Meta:
+        verbose_name = "Вариант тура"
+        verbose_name_plural = "Варианты туров"
 
 
 class TourVariantDetail(models.Model):
-	title = models.CharField(max_length=255, verbose_name="Заголовок")
-	description = models.CharField(max_length=255, verbose_name="Описание")
-	tour = models.ForeignKey(TourVariant, verbose_name="Тур", on_delete=models.CASCADE, related_name="details")
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.CharField(max_length=1024, verbose_name="Описание")
+    tour = models.ForeignKey(TourVariant, verbose_name="Тур", on_delete=models.CASCADE, related_name="details")
 
-	def __str__(self):
-		return f"{self.title} - {self.description}"
+    def __str__(self):
+        return f"{self.title} - {self.description}"
 
-	class Meta:
-		verbose_name = "Деталь тура"
-		verbose_name_plural = "Детали туров"
+    class Meta:
+        verbose_name = "Деталь тура"
+        verbose_name_plural = "Детали туров"
--- api/serializers.py
+++ api/serializers.py
@@ -1,126 +1,139 @@
-from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from api.models import TourVariantDetail, TourVariant, Company, CompanyContacts, Tour, CompanyFeed
 
 
 class ImageUrlField(serializers.RelatedField):
-	def to_representation(self, instance):
-		url = instance.photo.url
-		request = self.context.get('request', None)
-		if request is not None:
-			return request.build_absolute_uri(url)
+    def to_representation(self, instance):
+        url = instance.photo.url
+        request = self.context.get('request', None)
+        if request is not None:
+            return request.build_absolute_uri(url)
 
-		return url
+        return url
 
 
 class TourVariantDetailSerializer(ModelSerializer):
-	class Meta:
-		model = TourVariantDetail
-		fields = [
-			"title",
-			"description",
-		]
+    class Meta:
+        model = TourVariantDetail
+        fields = [
+            "title",
+            "description",
+        ]
 
 
 class CompanyContactsSerializer(ModelSerializer):
-	class Meta:
-		model = CompanyContacts
-		fields = [
-			"id",
-			"instagram",
-			"telegram",
-			"whatsapp",
-			"phone",
-		]
+    class Meta:
+        model = CompanyContacts
+        fields = [
+            "id",
+            "instagram",
+            "telegram",
+            "whatsapp",
+            "phone",
+        ]
 
 
 class CompanySerializer(ModelSerializer):
-	contacts = CompanyContactsSerializer()
+    contacts = CompanyContactsSerializer()
 
-	class Meta:
-		model = Company
-		fields = [
-			"id",
-			"name",
-			"contacts"
-		]
+    class Meta:
+        model = Company
+        fields = [
+            "id",
+            "name",
+            "contacts"
+        ]
 
 
 class CompanyFeedSerializer(ModelSerializer):
-	# photo = ImageUrlField(read_only=True)
-	photo = serializers.SerializerMethodField()
+    # photo = ImageUrlField(read_only=True)
+    photo = serializers.SerializerMethodField()
 
-	class Meta:
-		model = CompanyFeed
-		fields = [
-			"id",
-			"company",
-			"photo",
-			"feed"
-		]
+    class Meta:
+        model = CompanyFeed
+        fields = [
+            "id",
+            "name",
+            "company",
+            "photo",
+            "feed"
+        ]
 
-	def get_photo(self, feed):
-		request = self.context.get('request', None)
-		photo_url = feed.photo.url
-		if request is not None:
-			return request.build_absolute_uri(photo_url)
-		return photo_url
+    def get_photo(self, feed):
+        request = self.context.get('request', None)
+        photo_url = feed.photo.url
+        if request is not None:
+            return request.build_absolute_uri(photo_url)
+        return photo_url
 
 
 class TourVariantInlineSerializer(ModelSerializer):
-	company = CompanySerializer()
-	date = serializers.DateField(format="%d.%m.%Y", read_only=True)
-	details = TourVariantDetailSerializer(many=True)
+    company = CompanySerializer()
+    date = serializers.DateField(format="%d.%m.%Y", read_only=True)
+    details = TourVariantDetailSerializer(many=True)
 
-	class Meta:
-		model = TourVariant
-		fields = [
-			"company",
-			"coast",
-			"details",
-			"date",
-		]
+    class Meta:
+        model = TourVariant
+        fields = [
+            "id",
+            "tour",
+            "company",
+            "coast",
+            "date",
+            "difficulty",
+            "out_time",
+            "back_time",
+            "photographer",
+            "start_height",
+            "max_height",
+            "days_count",
+            "path_length_m",
+            "needed_items",
+            "details",
+        ]
 
 
 class TourSerializer(ModelSerializer):
-	photos = ImageUrlField(
-		many=True,
-		read_only=True,
-	)
-	variants = TourVariantInlineSerializer(many=True)
+    photos = ImageUrlField(
+        many=True,
+        read_only=True,
+    )
+    variants = TourVariantInlineSerializer(many=True)
 
-	class Meta:
-		model = Tour
-		fields = [
-			"id",
-			"photo",
-			"name",
-			"region",
-			"photos",
-			"variants",
-		]
+    class Meta:
+        model = Tour
+        fields = [
+            "id",
+            "photo",
+            "name",
+            "region",
+            "photos",
+            "variants",
+        ]
 
 
 class TourVariantSerializer(ModelSerializer):
-	out_time = serializers.DateTimeField(format="%d.%m.%Y %H:%M", read_only=True)
-	back_time = serializers.DateTimeField(format="%d.%m.%Y %H:%M", read_only=True)
+    out_time = serializers.DateTimeField(format="%d.%m.%Y %H:%M", read_only=True)
+    back_time = serializers.DateTimeField(format="%d.%m.%Y %H:%M", read_only=True)
 
-	company = CompanySerializer()
-	date = serializers.DateField(format="%d.%m.%Y", read_only=True)
-	details = TourVariantDetailSerializer(many=True)
+    company = CompanySerializer()
+    date = serializers.DateField(format="%d.%m.%Y", read_only=True)
+    details = TourVariantDetailSerializer(many=True)
 
-	class Meta:
-		model = TourVariant
-		fields = [
-			"id",
-			"tour",
-			"company",
-			"coast",
-			"details",
-			"date",
-			"out_time",
-			"back_time",
-			"difficulty",
-			"photographer",
-		]
+    class Meta:
+        model = TourVariant
+        fields = [
+            "id",
+            "tour",
+            "company",
+            "coast",
+            "details",
+            "date",
+            "out_time",
+            "back_time",
+            "difficulty",
+            "photographer",
+            "needed_items"
+        ]
