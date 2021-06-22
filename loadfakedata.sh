ca21ccb2913e50f4479da4bc84ba114079efbaee
sn1cka <ilenko.a99@gmail.com>
2021-06-22 23:33:57 +0600

    Added extra fields +Reworked serializers

--- loadfakedata.sh
+++ loadfakedata.sh
@@ -1,8 +1,8 @@
 set -e
-./manage.py loadtestdata api.Tour:10
-./manage.py loadtestdata api.TourPhoto:10
-./manage.py loadtestdata api.CompanyContacts:10
-./manage.py loadtestdata api.Company:10
+#./manage.py loadtestdata api.Tour:10
+#./manage.py loadtestdata api.TourPhoto:10
+#./manage.py loadtestdata api.CompanyContacts:10
+#./manage.py loadtestdata api.Company:10
 ./manage.py loadtestdata api.CompanyFeed:100
-./manage.py loadtestdata api.TourVariant:100
-./manage.py loadtestdata api.TourVariantDetail:200
+#./manage.py loadtestdata api.TourVariant:100
+#./manage.py loadtestdata api.TourVariantDetail:200
